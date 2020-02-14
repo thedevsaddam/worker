75e959ca969724efc3adb53da59f70ce9192ff38
Saddam H <thedevsaddam@gmail.com>
2020-02-15 00:59:26 +0600

    Sat Feb 15 00:59:26 +06 2020

--- example/main.go
+++ example/main.go
@@ -17,51 +17,51 @@ func main() {
 
 	log.Println("Enqueue task 1")
 	b.Register(func() {
-		log.Println("processing: tom 1")
+		// log.Println("processing: tom 1")
 		time.Sleep(duration)
-		log.Println("completed: tom 1")
+		// log.Println("completed: tom 1")
 	})
 
 	log.Println("Enqueue task 2")
 	b.Register(func() {
-		log.Println("processing: jerry 2")
+		// log.Println("processing: jerry 2")
 		time.Sleep(duration)
-		log.Println("completed: jerry 2")
+		// log.Println("completed: jerry 2")
 	})
 
 	log.Println("Enqueue task 3")
 	b.Register(func() {
-		log.Println("processing: john 3")
+		// log.Println("processing: john 3")
 		time.Sleep(duration)
-		log.Println("completed: john 3")
+		// log.Println("completed: john 3")
 	})
 
 	log.Println("Enqueue task 4")
 	b.Register(func() {
-		log.Println("processing: joe 4")
+		// log.Println("processing: joe 4")
 		time.Sleep(duration)
-		log.Println("completed: joe 4")
+		// log.Println("completed: joe 4")
 	})
 
 	log.Println("Enqueue task 5")
 	b.Register(func() {
-		log.Println("processing: jack 5")
+		// log.Println("processing: jack 5")
 		time.Sleep(duration)
-		log.Println("completed: jack 5")
+		// log.Println("completed: jack 5")
 	})
 
 	log.Println("Enqueue task 6")
 	b.Register(func() {
-		log.Println("processing: sparrow 6")
+		// log.Println("processing: sparrow 6")
 		time.Sleep(duration)
-		log.Println("completed: sparrow 6")
+		// log.Println("completed: sparrow 6")
 	})
 
 	log.Println("Enqueue task 7")
 	b.Register(func() {
-		log.Println("processing: witcher 7")
+		// log.Println("processing: witcher 7")
 		time.Sleep(duration)
-		log.Println("completed: witcher 7")
+		// log.Println("completed: witcher 7")
 	})
 
 	// run background worker
